a98870f16e26641e1034786f29026b867c5692f1
blendi <dronramone@mail.ru>
2023-02-14 20:29:16 +0600

    Добавлена сортировка операций по дате

--- funktion.py
+++ funktion.py
@@ -19,6 +19,7 @@ def create_ex_class():
     """
     list_ex_class = []
     list_from_json = get_json_to_list()
+    list_from_json.sort(key=lambda d: d['date'], reverse=True)
     for i in list_from_json:
         if 'from' in i.keys():
             list_ex_class.append(Operation(i['date'],
