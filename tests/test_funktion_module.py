a2db90b7939fad2395b5d879e93c010198b0f036
blendi <dronramone@mail.ru>
2023-02-17 20:59:05 +0600

    Тест функции создания списка классов

--- tests/test_funktion_module.py
+++ tests/test_funktion_module.py
@@ -1,7 +1,13 @@
-from funktion import get_json_to_list
+import funktion as funk
 
 
 def test_get_json():
     """Тест get_json_to_list"""
-    assert type(get_json_to_list()) == list
-    assert get_json_to_list() is not None
+    assert type(funk.get_json_to_list()) == list
+    assert funk.get_json_to_list() is not None
+
+
+def test_create_ex_class():
+    """Тест функции создания списка экземпляра класса класса"""
+    assert type(funk.create_ex_class()) == list
+    assert funk.create_ex_class() is not None
